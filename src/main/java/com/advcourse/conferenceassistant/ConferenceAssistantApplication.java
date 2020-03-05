8492ab08619b0d66942ec0a835c15e219a719b1a
kAeva <ksenia.aeva@gmail.com>
2020-03-05 16:07:36 +0200

    Initial commit

---
+++ src/main/java/com/advcourse/conferenceassistant/ConferenceAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.advcourse.conferenceassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/advcourse/conferenceassistant/ConferenceAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advcourse.conferenceassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
